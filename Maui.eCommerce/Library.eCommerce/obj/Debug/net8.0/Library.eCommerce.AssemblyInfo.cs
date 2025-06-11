eb088fa4e6cba16ba989b3195be868bacc5dbc8b
Griz0372 <amt22m@fsu.edu>
2025-06-11 16:00:42 -0400

    Add cart selection feature

--- Maui.eCommerce/Library.eCommerce/obj/Debug/net8.0/Library.eCommerce.AssemblyInfo.cs
+++ Maui.eCommerce/Library.eCommerce/obj/Debug/net8.0/Library.eCommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.eCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57b47398043681004c979169544615d9390ffb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6dc3d49a6e5977f2abe50f16ae0633dd6e44857")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.eCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.eCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Maui.eCommerce/Maui.eCommerce/MainPage.xaml.cs
+++ Maui.eCommerce/Maui.eCommerce/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Maui.eCommerce;
+﻿using Library.eCommerce.Services;
+
+namespace Maui.eCommerce;
 
 public partial class MainPage : ContentPage
 {
@@ -11,20 +13,49 @@ public partial class MainPage : ContentPage
 
     private void InventoryClicked(object sender, EventArgs e)
     {
-        Shell.Current.GoToAsync("//InventoryManagement");
+        Shell.Current.GoToAsync("//InventoryManagementView");
     }
 
     private void ShopClicked(object sender, EventArgs e)
     {
-        Shell.Current.GoToAsync("//InventoryManagement");
+        Shell.Current.GoToAsync("//InventoryManagementView");
     }
     
     private void CartClicked(object sender, EventArgs e)
     {
-        Shell.Current.GoToAsync("//ShoppingCart");
+        Shell.Current.GoToAsync("//ShoppingCartView");
     }
     private void ConfigClicked(object sender, EventArgs e)
     {
-        Shell.Current.GoToAsync("//Configuration");
+        Shell.Current.GoToAsync("//ConfigurationView");
+    }
+    
+    private void CartSelected(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        string buttonText = button.Text;
+    
+        if (int.TryParse(buttonText.Replace("Cart ", ""), out int cartId))
+        {
+            try
+            {
+                for (int i = 0; i <= cartId; i++)
+                {
+                    if (!CartManagerService.Current.CartItems.ContainsKey(i))
+                    {
+                        ShoppingCartService newCart = new ShoppingCartService();
+                        CartManagerService.Current.CartItems.Add(i, newCart);
+                    }
+                }
+            
+                CartManagerService.Current.SwitchActiveCart(cartId);
+            
+                DisplayAlert("Cart Selected", $"You are now using {buttonText}", "OK");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
     }
 }
